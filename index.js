986f77073a343ad26ad09760b28cb8a6a15a3495
Tia Rose <shortyshort789@aol.com>
2019-06-24 12:07:33 -0700

    working on getting Heroku 'Hello World' working

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Hello Page');
+});
+
+module.exports = {
+  server: app,
+  start: port =>{
+    let PORT = process.env.PORT || port;
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  },
+};
