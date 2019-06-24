d40635837d01ba462b4b01557816b70a2ce0fc96
Tia Rose <shortyshort789@aol.com>
2019-06-24 12:56:02 -0700

    working on heroku deployment

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 'use strict';
 
-require('dotenv').config();
+const server = require('./api');
 
-const express = require('express');
-const app = express();
-
-app.get('/', (request, response) => {
-  response.send('Hello World');
-});
-
-module.exports = {
-  server: app,
-  start: port =>{
-    let PORT = process.env.PORT || port;
-    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-  },
-};
+server.start(process.env.PORT);
